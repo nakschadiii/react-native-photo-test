cf1be565465f8f230b604a8a19f32ccbdf579a42
nakschadiii <105643348+nakschadiii@users.noreply.github.com>
2023-11-06 16:51:06 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import { Camera } from 'expo-camera';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import { Camera, CameraType } from 'expo-camera';
+import { useState } from 'react';
 
 export default function App() {
-    const [permission, requestPermission] = Camera.useCameraPermissions();
+	const [permission, requestPermission] = Camera.useCameraPermissions();
+	const [cameraMode, setCameraMode] = useState(true);
+	const [uri, setUri] = useState('https://reactnative.dev/img/tiny_logo.png');
 
     if (!permission) {
-        // Camera permissions are still loading
         return <View />;
     }
 
+	takePicture = () => {
+		if (this.camera) {
+			this.camera.takePictureAsync({ onPictureSaved: this.onPictureSaved });
+		}
+	 };
+  
+	onPictureSaved = photo => {
+		setUri(photo.uri);
+		console.log(photo.uri);
+	} 
+
     if (!permission.granted) {
-        // Camera permissions are not granted yet
         return (
             <View style={styles.container}>
                 <Text>We need your permission to show the camera</Text>
@@ -22,34 +34,35 @@ export default function App() {
         );
     }
 
-    return (
-        <View style={styles.container}>
-            <Text>KOLA</Text>
-            <Camera style={styles.camera} type={Camera.Constants.Type.front} />
-            <TouchableOpacity
-                onPress={() => console.log('Button Pressed')}
-                style={styles.btnSection}>
+	return (
+		<View style={styles.container}>
+			<Camera style={styles.camera} type={CameraType.back} ref={(ref) => { this.camera = ref }} ></Camera>
+			<TouchableOpacity onPress={() => takePicture()} style={styles.btnSection}>
                 <Text style={styles.btnText}>Directly Launch Camera</Text>
             </TouchableOpacity>
-        </View>
-    );
+			<Image style={styles.stretch} source={{ uri: uri, }} ref={(ref) => { this.preview = ref }} />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    camera: {
-        height: '100%',
-        width: "100%",
-    },
-    btnSection: {
-        // Define your button styles here
-    },
-    btnText: {
-        // Define your button text styles here
-    }
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	camera : {
+		height: 'fit-content',
+		width: "100%",
+		aspectRatio: "3/4",
+		objectFit: 'cover',
+	},
+	stretch: {
+		width: 200,
+		height: 200,
+		aspectRatio: "3/4",
+		resizeMode: 'stretch',
+		objectFit: "cover",
+	},
 });
