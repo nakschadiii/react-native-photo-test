582b1b9410a4c18e4968e17c3dd9fba39b06b5b0
DocteurSEO <38159831+DocteurSEO@users.noreply.github.com>
2023-11-06 16:03:01 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import { Camera, CameraType } from 'expo-camera';
+import { Camera } from 'expo-camera';
 
 export default function App() {
-	const [permission, requestPermission] = Camera.useCameraPermissions();
-	
-	return (
-		<View style={styles.container}>
-			<Text>KOLA</Text>
-			<Camera style={styles.camera} type={CameraType.front}></Camera>
-			<TouchableOpacity
-                onPress={() => console.log('Response = ')}
+    const [permission, requestPermission] = Camera.useCameraPermissions();
+
+    if (!permission) {
+        // Camera permissions are still loading
+        return <View />;
+    }
+
+    if (!permission.granted) {
+        // Camera permissions are not granted yet
+        return (
+            <View style={styles.container}>
+                <Text>We need your permission to show the camera</Text>
+                <TouchableOpacity onPress={requestPermission} style={styles.btnSection}>
+                    <Text style={styles.btnText}>Grant Permission</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text>KOLA</Text>
+            <Camera style={styles.camera} type={Camera.Constants.Type.front} />
+            <TouchableOpacity
+                onPress={() => console.log('Button Pressed')}
                 style={styles.btnSection}>
                 <Text style={styles.btnText}>Directly Launch Camera</Text>
             </TouchableOpacity>
-		</View>
-	);
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	camera : {
-		height: '100%',
-		width: "100%",
-	}
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    camera: {
+        height: '100%',
+        width: "100%",
+    },
+    btnSection: {
+        // Define your button styles here
+    },
+    btnText: {
+        // Define your button text styles here
+    }
 });
